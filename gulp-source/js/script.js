4f75da321b0747baa0f127e63dfa9e38dad425ad
Alexei Danilovich <a.danilovich@kakadu.bz>
2019-12-03 19:29:30 +0300

    migrate repo

---
+++ gulp-source/js/script.js
@@ -0,0 +1,65 @@
+const width  = document.querySelector('#width')
+const height = document.querySelector('#height')
+
+const element = document.querySelector('#element')
+const result  = document.querySelector('#result')
+
+const allInputs  = document.querySelectorAll('input')
+const allButtons = document.querySelectorAll('.button')
+
+let activeButton = 'wp'
+let calculated
+
+function toggleActiveButton() {
+    if (!this.classList.contains('active')) {
+        allButtons.forEach(button => {
+            button.classList.remove('active')
+        })
+
+        this.classList.add('active')
+    }
+
+    activeButton = this.id
+
+    getTotalValue()
+}
+
+function calculate() {
+    let currentNumberForCalculate
+
+    if (activeButton === 'wp' || activeButton === 'fs') {
+        currentNumberForCalculate = width.value.toString()
+    } else (
+        currentNumberForCalculate = height.value.toString()
+    )
+
+    calculated = element.value.toString() / currentNumberForCalculate * 100;
+
+    if (activeButton === 'wp' || activeButton === 'fs') {
+        result.value = activeButton + '(' + calculated.toFixed(3) + '), // ' + element.value
+    } else {
+        result.value = activeButton + '(' + calculated.toFixed(3) + '), // ' + element.value
+    }
+}
+
+function copyTotal(str) {
+
+    let tmp   = document.createElement('INPUT'), // Создаём новый текстовой input
+        focus = document.activeElement // Получаем ссылку на элемент в фокусе (чтобы не терять фокус)
+
+    tmp.value = str; // Временному input вставляем текст для копирования
+
+    document.body.appendChild(tmp) // Вставляем input в DOM
+    tmp.select() // Выделяем весь текст в input
+    document.execCommand('copy') // Магия! Копирует в буфер выделенный текст (см. команду выше)
+    document.body.removeChild(tmp) // Удаляем временный input
+    focus.focus() // Возвращаем фокус туда, где был
+}
+
+function getTotalValue() {
+    calculate()
+    copyTotal(result.value)
+}
+
+allButtons.forEach(button => button.addEventListener('click', toggleActiveButton))
+allInputs.forEach(input => input.addEventListener('change', getTotalValue))
---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+const gulp         = require('gulp'),
+      sass         = require('gulp-sass'),
+      browsersync  = require('browser-sync').create(),
+      del          = require('del'),
+      autoprefixer = require('gulp-autoprefixer'),
+      csso         = require('gulp-csso'),
+      pug          = require('gulp-pug'),
+      htmlmin      = require('gulp-htmlmin');
+
+const path = {
+    build:    './src',
+    css:      {
+        source:      './gulp-source/styles/main.+(scss|sass)',
+        dest:        './src/',
+        watchSource: './gulp-source/styles/**/*.scss',
+    },
+    html:     {
+        indexSource: './gulp-source/pages/index.pug',
+        dest:        './src/',
+        watchSource: './gulp-source/pages/**/*.pug',
+    },
+    scripts:  {
+        source:      './gulp-source/js/**/*',
+        dest:        './src/',
+        watchSource: './gulp-source/js/**/*.js',
+    },
+    electron: {
+        source: './electron-config/**/*',
+        dest:   './src/',
+    },
+};
+
+// Clean
+gulp.task('clean', done => {
+    del.sync(path.build);
+    done();
+});
+
+// Css
+gulp.task('css', done => {
+    gulp
+        .src(path.css.source)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(['last 5 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+        .pipe(csso())
+        .pipe(gulp.dest(path.css.dest))
+        .pipe(browsersync.stream());
+    done();
+});
+
+// Html
+gulp.task('html', done => {
+    gulp
+        .src(path.html.indexSource)
+        .pipe(pug())
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(path.build))
+        .pipe(browsersync.stream());
+    done();
+});
+
+// Scripts
+gulp.task('scripts', done => {
+    gulp
+        .src(path.scripts.source)
+        .pipe(gulp.dest(path.scripts.dest));
+    done();
+});
+
+// Electron
+gulp.task('electron', done => {
+    gulp
+        .src(path.electron.source)
+        .pipe(gulp.dest(path.electron.dest));
+    done();
+});
+
+// BrowserSync
+function reload(done) {
+    browsersync.reload();
+    done();
+}
+
+gulp.task('browser-sync', done => {
+    browsersync.init({
+        server: {
+            baseDir: path.build,
+        },
+        notify: false,
+    });
+    done();
+});
+
+// Watch files
+gulp.task('watch', done => {
+    gulp.watch(path.css.watchSource, gulp.series('css', reload));
+    gulp.watch(path.html.watchSource, gulp.series('html', reload));
+    gulp.watch(path.scripts.watchSource, gulp.series('scripts', reload));
+    done();
+});
+
+gulp.task('default', gulp.parallel('clean', 'electron', 'css', 'html', 'scripts', 'browser-sync', 'watch'));
