e2cadbabcaee46b9200e85e4385052b75d792d94
AlexDanilovich <alexeidanilovich031193@gmail.com>
2022-10-22 20:43:41 +0300

    feat: changed app window size

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,8 +71,9 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 440,
+    height: 285,
+    autoHideMenuBar: true,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
