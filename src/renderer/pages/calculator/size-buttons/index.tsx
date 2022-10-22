8719a77bc3988acdefa6d924382148e5a327e198
AlexDanilovich <alexeidanilovich031193@gmail.com>
2022-10-22 20:30:00 +0300

    feat: added hotkeys to change active size button

--- src/renderer/pages/calculator/size-buttons/index.tsx
+++ src/renderer/pages/calculator/size-buttons/index.tsx
@@ -1,9 +1,15 @@
 import type { FC, MouseEvent } from 'react';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Button } from 'semantic-ui-react';
 import SIZE from '@interfaces/size';
 import type { IStore } from './index.stores';
 import styles from './styles.module.scss';
+
+const keyToSIZE = {
+  q: SIZE.WIDTH,
+  w: SIZE.HEIGHT,
+  e: SIZE.FONT,
+} as const;
 
 /**
  * SizeButtons
@@ -12,11 +18,43 @@ import styles from './styles.module.scss';
 const SizeButtons: FC<IStore> = ({
   store: { activeSizeButton, setActiveSizeButton },
 }) => {
+  /**
+   * On click button
+   */
   const onClick = useCallback(
     (e: MouseEvent<HTMLButtonElement>) =>
       void setActiveSizeButton(e.currentTarget.dataset.value as SIZE),
     [setActiveSizeButton]
   );
+
+  /**
+   * Keydown listener
+   */
+  const onKeydown = useCallback(
+    (e: KeyboardEvent) => {
+      if (!e.ctrlKey) {
+        return;
+      }
+
+      const size = keyToSIZE?.[e.key as keyof typeof keyToSIZE];
+
+      if (!size) {
+        return;
+      }
+
+      setActiveSizeButton(size);
+    },
+    [setActiveSizeButton]
+  );
+
+  /**
+   * Add keydown listener event
+   */
+  useEffect(() => {
+    window.addEventListener('keydown', onKeydown);
+
+    return () => window.removeEventListener('keydown', onKeydown);
+  }, [onKeydown]);
 
   return (
     <div>
