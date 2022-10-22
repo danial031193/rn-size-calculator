d78ed2ce7c1dd8f15c90b2781adc61fb09b5956b
AlexDanilovich <alexeidanilovich031193@gmail.com>
2022-10-22 13:22:46 +0300

    chore: fixed ci

--- src/renderer/typings/common.d.ts
+++ src/renderer/typings/common.d.ts
@@ -1,2 +1,28 @@
-import '@lomray/client-helpers-react/typings/scss';
-import '@lomray/client-helpers-react/typings/images';
+type Styles = Record<string, string>;
+
+declare module '*.svg' {
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: Styles;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: Styles;
+  export default content;
+}
